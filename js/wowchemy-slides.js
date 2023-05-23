b1e86b504076672820a5f8868be7ea00e3f2afc7
LuminolT <LuminolT@users.noreply.github.com>
2023-05-23 18:06:16 +0000

    deploy: LuminolT/starter-hugo-academic@b3d28335dfe1731324e56dbee64f829fa0c166fd

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-params:@params
-  var slides = { highlight_style: "dracula", theme: "black" };
+  var slides = { center: false, highlight_style: "github-dark", "slide center": false, theme: "black" };
 
   // <stdin>
   var enabledPlugins = [RevealMarkdown, RevealSearch, RevealNotes, RevealMath.MathJax3, RevealZoom];
